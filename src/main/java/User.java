6340c88089056d1edca0b1c2cc8e1172a098fbc2
sunkl <970910938@qq.com>
2018-09-14 15:36:45 +0800

    开发2

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -5,8 +5,8 @@ public class User {
     private String name;
     private String password;
    private String 我是一个测试的东西;
-   public void test(){
-       System.out.println("111111");
+   public void teee(){
+       System.out.println(22222122);
    }
 
 }
