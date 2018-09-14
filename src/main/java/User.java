450db5c1116d5e02f147a9604ba92312c66ca468
sunkl <970910938@qq.com>
2018-09-14 15:55:01 +0800

    开发支线

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -4,7 +4,7 @@
 public class User {
     private String name;
     private String password;
-   private String 当前为本地主线;
+   private String 当前为本地支线;
    public void teee(){
        System.out.println(22222122);
    }
